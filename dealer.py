ad0be8d68f7f4261a48f6b5f4d79b3d76ad9bb7f
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-03-02 22:56:27 -0600

    working random shuffler

--- dealer.py
+++ dealer.py
@@ -1,18 +1,21 @@
 from deck import Deck
+from card import Card
 import random
 
 class Dealer(object):
     
     def __init__(self):
-        self.work()
+        self.combineDecks()
         
     def __str__(self):
-        return(f'''{self.megaDeck}''')
+        return '[' + ', '.join(str(card) for card in self.megaDeck) + ']'
     
     def combineDecks(self):
         self.megaDeck = []
         for i in range(4):
-            self.megaDeck.append(Deck())
+            for i in range(4):
+                for j in range(0,14):
+                    self.megaDeck.append(Card(i, j))
     
     def shuffle(self):
         n = len(self.megaDeck)
@@ -22,12 +25,12 @@ class Dealer(object):
             
     def deal(self):
         self.dealtCards = []
+        self.cardDealt = self.megaDeck[0]
+        print(f'''Card Dealt: {self.cardDealt}''')
         self.dealtCards.append(self.megaDeck[0])
-        return self.megaDeck[0]
         self.megaDeck.pop(0)
-        
-    def work(self):
-        responce = input(f"Deal one card? Y or N: ")
-        if responce == "Y":
-            self.deal()  
-          
+    
+    def recycle(self):
+        for i in range(len(self.dealtCards)):
+            self.megaDeck.append(self.dealtCards[i])
+        self.shuffle()
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from dealer import Dealer
-from deck import Deck
+dealer1 = Dealer()
 
-deck1 = Deck()
-deck1.shuffle()
-deck1.sort()
-print(deck1)
-
-deal = Dealer()
-print(str(deal))
+def menu():
+    dealerChoice = input(f'''
+                (1). Shuffle
+                (2). Deal a Card
+                (3). Recycle Dealt Cards
+                Enter Choice: ''')
+    if(dealerChoice == '1'):
+        dealer1.shuffle()
+        print("Shuffled")
+    elif(dealerChoice == '2'):
+        choice = 'Y'
+        while(choice == 'Y' or choice == 'y'):
+            dealer1.deal()
+            choice = str(input("Another?(Y or N) "))
+    elif(dealerChoice == '3'):
+        dealer1.recycle()
+        print("Cards Returned + Shuffled")
+        
+backtomenu = 'Y'
+while(backtomenu == 'Y' or backtomenu == 'y'):
+    menu()
+    backtomenu = str(input("Back to Menu?(Y or N) "))
